273d188509a0e36333adb4e1d6acb9e95de8b982
Catalin Lazar <catalin.lazar@endava.com>
2017-08-08 16:12:12 +0300

    Solve

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,26 +3,61 @@
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Solution {
     
     private final Scanner in;
     private final PrintStream out;
     private final String[] args;
+    private final int n;
+    private final int d;
     
     public Solution(InputStream in, OutputStream out, String[] args) {
         this.in = new Scanner(in);
         this.out = new PrintStream(out);
         this.args = args;
+        this.n = this.in.nextInt();
+        this.d = this.in.nextInt();
     }
 
     public static void main(String[] args) {
         new Solution(System.in, System.out, args).solve();
     }
 
-    public void solve() {
-        // TODO Auto-generated method stub
+    static class Rotator {
+        private final int[] rotated;
+        private int currentPosition = 0;
         
+        public Rotator(int... original) {
+            rotated = new int[original.length * 2];
+            System.arraycopy(original, 0, rotated, 0, original.length);
+            System.arraycopy(original, 0, rotated, original.length, original.length);
+        }
+
+        public void rotateLeft() {
+            rotateLeft(1);
+        }
+        
+        public void rotateLeft(int d) {
+            currentPosition += d;
+        }
+
+        public List<Integer> getRotated() {
+            List<Integer> get = new ArrayList<>(rotated.length / 2);
+            IntStream.range(currentPosition, currentPosition + rotated.length / 2).map(i -> rotated[i]).forEach(get::add);
+            return get;
+        }
+    }
+    
+    public void solve() {
+        int[] arr = new int[n];
+        IntStream.range(0, n).forEach(i -> arr[i] = in.nextInt());
+        Rotator rotator = new Rotator(arr);
+        rotator.rotateLeft(d);
+        out.print(rotator.getRotated().toString().replaceAll("[\\[\\],]",""));
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -5,6 +5,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 import org.assertj.core.api.AbstractCharSequenceAssert;
 import org.junit.After;
@@ -31,7 +33,7 @@ public class SolutionTest {
             }
         }
     }
-    
+
     private Solution withInput(String input) {
         in = new ByteArrayInputStream(input.getBytes(Charset.forName("UTF-8")));
         return new Solution(in, out, new String[]{});
@@ -51,4 +53,29 @@ public class SolutionTest {
         return assertThat(new String(out.toByteArray(), Charset.forName("UTF-8")));
     }
     
+    @Test
+    public void rotatesLeftOnce() throws Exception {
+        Solution.Rotator rotator = new Solution.Rotator(1, 2, 3, 4, 5);
+        
+        rotator.rotateLeft();
+        
+        assertThat(rotator.getRotated()).isEqualTo(Arrays.asList(2, 3, 4, 5, 1));
+    }
+    
+    @Test
+    public void solvesInput() throws Exception {
+        withInputFile().solve();
+        
+        assertOutput().isEqualTo("5 1 2 3 4");
+    }
+    
+    @Test(timeout = 1000)
+    public void solvesBigInputInTimelyFashion() throws Exception {
+        int n = 10000;
+        
+        StringBuilder sb = new StringBuilder().append(n).append(" ").append(n).append("\n");
+        IntStream.range(0, n).forEach(i -> sb.append(i).append(" "));
+        
+        withInput(sb.toString()).solve();
+    }
 }
